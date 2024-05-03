4e54ffc983af88d9616ee3af0a4c1793c64380f2
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-05-03 12:52:08 +0900

    add: 文字かどうかの判定メソッド

--- app/src/Helpers/ValidationHelper.php
+++ app/src/Helpers/ValidationHelper.php
@@ -9,4 +9,11 @@ class ValidationHelper
   {
     return is_numeric($value);
   }
+
+  public static function isPureString(string $value): bool
+  {
+    // 正規表現パターン: 文字列は、数字、記号、空白を含まずに構成される必要があります。
+    $pattern = '/^[^\d\s!@#$%^&*()_+=\-[\]{};:"\\|,.<>\/?]*$/';
+    return preg_match($pattern, $value) === 1;
+  }
 }
