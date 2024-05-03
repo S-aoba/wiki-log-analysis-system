e4d74982967de48ba121e03637232b3060aa5998
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-05-03 12:28:30 +0900

    add: validation.

--- app/src/TopPageViews.php
+++ app/src/TopPageViews.php
@@ -1,17 +1,43 @@
 <?php
 
+use Helpers\ValidationHelper;
+
 require_once(__DIR__ . '/Interface/Task.php');
 require_once(__DIR__ . '/DB.php');
 require_once(__DIR__ . '/Log.php');
+
+require_once(__DIR__ . '/Helpers/ValidationHelper.php');
 
 class TopPageViews implements Task
 {
   public function execute(DB $db)
   {
-    // 指定した記事数分だけビュー数が多い順にソートして表示するタスク
-    echo "件数を入力してください: ";
-    $limit = trim(fgets(STDIN));
+    // 表示する件数を取得
+    $limit = $this->getLimitNumberByUser();
+
+    // データベースからデータを取得する
     $res = $db->getTopPageViews($limit);
+
+    // ターミナルに表示する
     Log::displayTopPageViews($res);
+  }
+
+  private function getLimitNumberByUser(): int
+  {
+    $limit = 3;
+    while (true) {
+      echo "件数を入力してください: ";
+      $limit = trim(fgets(STDIN));
+
+      // 入力された値が数値以外であるかどうかを検証する
+      $isNumber = ValidationHelper::isNumber($limit);
+
+      if ($isNumber) {
+        break;
+      }
+      echo "不正な値が入力されました" . PHP_EOL;
+    }
+
+    return $limit;
   }
 }
